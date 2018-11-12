2879cc22c533406ad692f551162b8f418fa5e5ac
Aleksandr Nikitin <nikitinal.nal@gmail.com>
2018-11-12 17:16:44 +0300

    add array and items shedule

---
+++ blocks/item-shedule/array.js
@@ -0,0 +1,65 @@
+let options = [];
+options.push({
+              type: 'Glasses',
+              price: 1500 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Another Glasses',
+              price: 850 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Used Glasses',
+              price: 320 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Broken Glasses old edition',
+              price: 150 + ' руб.',
+              condition: null,
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Glasses new edition',
+              price: 1650 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Another Glasses',
+              price: 430 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Used broken Glasses',
+              price: 222 + ' руб.',
+              condition: 'available',
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
+options.push({
+              type: 'Glasses with broken package',
+              price: 70 + ' руб.',
+              condition: null,
+              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique suscipit facilis praesentium perferendis sunt culpa adipisci inventore quis laboriosam hic, doloremque, voluptatem earum placeat possimus architecto expedita impedit fuga explicabo!',
+              avatar: './images/glasses.jpg'
+            }
+          );
--- blocks/item-shedule/item-sh.js
+++ blocks/item-shedule/item-sh.js
@@ -1,13 +1,26 @@
+window.addEventListener('DOMContentLoaded', function () {
+for (let i = 0; i < options.length; i++){  
+  let optionsElement = options[i];
+  let item = new Item({el: document.querySelector('.product-container'), optionsElement});
+
+  item.render();
+}
+
+});
+
+
+
 (function () {
   'use strict';
 
 class Item {
-  constructor({el, options}){
+  constructor({el, optionsElement}){
     this.el = el;
-    this.type = options.type;
-    this.desc = options.desc;
-    this.avatar = options.avatar;
-    this.price = options.price;
+    this.type = optionsElement.type;
+    this.desc = optionsElement.desc;
+    this.avatar = optionsElement.avatar;
+    this.price = optionsElement.price;
+    this.cond = optionsElement.condition;
  
   }
 
@@ -15,27 +28,27 @@ class Item {
 
   render(){
   let el = this.el;
-  let div = document.createElement('div');
-  div.innerHTML =  `<div class="item pure-g">
+  let li = document.createElement('li');
+  li.innerHTML =  `<div class="item pure-g">
                     <div class="pure-u"></div>
                     <div class="item-body"><h4 class="type"></h4>
                     <p class="description"></p></div>
                     <button class="button">${this.price}</button></div>`;
-  el.appendChild(div);
-  div.querySelector('.pure-u').innerHTML = `<img  class="avatar">`
-  div.querySelector('.avatar').setAttribute('src', this.avatar);
-  div.querySelector('.type').innerHTML = this.type;
+  el.appendChild(li);
+  li.querySelector('.pure-u').innerHTML = `<img  class="avatar">`
+  li.querySelector('.avatar').setAttribute('src', this.avatar);
+  li.querySelector('.type').innerHTML = this.type;
   
   //onmouseover
-  div.addEventListener('mouseover', function (e){
-    div.classList.toggle('item-hover');
+  li.addEventListener('mouseover', function (e){
+    li.classList.toggle('item-hover');
   })
   //onmouseout
-  div.addEventListener('mouseout', function (e){
-    div.classList.toggle('item-hover');
+  li.addEventListener('mouseout', function (e){
+    li.classList.toggle('item-hover');
   })
   //onclick
-  div.querySelector('.item-body').addEventListener('click', function (e){
+  li.querySelector('.item-body').addEventListener('click', function (e){
     let elClick = e.target.closest('.item');
 
     let selected = document.querySelector('.item-selected');
@@ -48,11 +61,13 @@ class Item {
 
   })
     //order
-  div.querySelector('.button').addEventListener('click', function(e){
+  let price  = this.price;
+  li.querySelector('.button').addEventListener('click', function(e){
     let button = event.target.closest('.button');
     if(button) {
       if(button.innerHTML == 'ordered'){
-        button.innerHTML = 'order now';
+
+        button.innerHTML = price;
         button.classList.remove('button-active');
       } else{
         button.innerHTML = 'ordered';
@@ -62,25 +77,26 @@ class Item {
   })
 
   // avataar width
-  let avatar = div.querySelector('.pure-u');
+  let avatar = li.querySelector('.pure-u');
   avatar.style.width = 70 + 'px'; 
 
 
   // destription cutter
+    let elWidth = el.offsetWidth;
     if( el.offsetWidth < 720) {
       let string = this.desc;
 
-      let limitStringLength = 50; // description string length;
+      let limitStringLength = elWidth/6; // description string length;
       if(string.length > limitStringLength) {
 
         string = string.slice(0, limitStringLength-3);
         let whiteSpaceNumber = string.lastIndexOf(' ');
         this.desc = string.slice(0, whiteSpaceNumber);
-        div.querySelector('.description').innerHTML = this.desc + ' ...';
+        li.querySelector('.description').innerHTML = this.desc + ' ...';
 
       }      
       } else {
-        div.querySelector('.description').innerHTML = this.desc;  
+        li.querySelector('.description').innerHTML = this.desc;  
         
     }
   }
