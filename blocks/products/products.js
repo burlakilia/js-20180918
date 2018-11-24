218180c171212503277ad139be00654a571b57d4
Dmitrii Levchenko <dlxeon@gmail.com>
2018-11-24 14:39:27 +0300

    Рефакторинг

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -4,30 +4,23 @@ import template from './products.pug';
 export class Products extends Component {
     constructor(el, options) {
         super(el, options);
-        let _clickAdded;
-        this.getClickAdded = () => _clickAdded;
-        this.setClickAdded = (added) => _clickAdded = added;
-    }
-    
-    render(data) {
-        this.el.innerHTML = template(data);
-        const that = this;
-        if (!this.getClickAdded()) {
-            this.setClickAdded(true);
-            this.el.addEventListener('click', (e) => {
-                const guid = tryFindProductGuid(e.srcElement);
-                if (guid) {
-                    if (this.onItemClick) {
-                        this.onItemClick(guid);
-                    }
-                    // Set active
-                    setActiveProduct(this.el, guid);
-                }
-            });
-            this.clickAdded = true;
-        }
-        setActiveProduct(this.el, data.selectedProductId);
 
+        this.el.addEventListener('click', (e) => {
+            const guid = tryFindProductGuid(e.srcElement);
+            if (guid) {
+                if (this.onItemClick) {
+                    this.onItemClick(guid);
+                }
+                // Set active
+                setActiveProduct(this.el, guid);
+            }
+        });
+        
+        this.render = function(data) {
+            this.el.innerHTML = template(data);
+            setActiveProduct(this.el, data.selectedProductId);
+        }
+        
         function setActiveProduct(el, selectedGuid) {
             if (selectedGuid) {
                 for (const itemElem of el.querySelectorAll('.products__item')) {
