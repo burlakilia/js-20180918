fcfa8fd30d0b77dd9cd50557475968d5391aa6fd
Dmitrii Levchenko <dlxeon@gmail.com>
2018-11-27 20:43:38 +0300

    Avoid code duplication

--- core/productsApi.js
+++ core/productsApi.js
@@ -1,12 +1,10 @@
 export class ProductsApi{
-    // https://murmuring-cove-73426.herokuapp.com/
     constructor() {
         const host = "https://murmuring-cove-73426.herokuapp.com/";
         
-        this.getProducts = function (onSuccess, onError) {
-            const url = host + "products";
+        const doRequest = function (method, url, onSuccess, onError) {
             const request = new XMLHttpRequest();
-            request.open('GET', url);
+            request.open(method, url);
             request.onreadystatechange = () => {
                 if(request.readyState === 4) {
                     if (request.status === 200) {
@@ -19,23 +17,13 @@ export class ProductsApi{
             request.onerror = () => onError();
             request.send();
         };
+        
+        this.getProducts = function (onSuccess, onError) {
+            doRequest("GET", host + "products", onSuccess, onError);
+        };
 
         this.getProductInfo = function (productId, onSuccess, onError) {
-            const url = host + "products/" + productId;
-            const request = new XMLHttpRequest();
-            request.open('GET', url);
-            request.onreadystatechange = () => {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
-                        onSuccess(request.responseText);
-                    } else {
-                        onError();
-                    }
-                }
-            };
-
-            request.onerror = () => onError();
-            request.send();
+            doRequest("GET", host + "products/" + productId, onSuccess, onError);
         };
     }
 }
